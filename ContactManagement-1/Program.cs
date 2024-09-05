cd1f8582fe9aefbed55e813a371f2aa2298b71a0
ASEELALOMARI <392206377@qu.edu.sa>
2024-09-05 21:18:38 +0300

    solved Problem 1 and 2

---
+++ ContactManagement-1/Program.cs
@@ -0,0 +1,101 @@
+﻿public record Contact(string Name, string PhoneNumber, string Email);
+
+class ContactManager
+{
+
+    private static List<Contact> contacts = new List<Contact>();
+
+
+    public void AddContact(Contact contactX)
+    {
+        contacts.Add(contactX);
+        Console.WriteLine($"Contact {contactX.Name} added successfully.");
+    }
+
+
+
+    public void RemoveContact(string name)
+    {
+        Contact? contactToRemove = contacts.FirstOrDefault(contactName => contactName.Name == name);
+        if (contactToRemove != null)
+        {
+
+            contacts.Remove(contactToRemove);
+            Console.WriteLine($"Contact {contactToRemove.Name} removed successfully.");
+
+        }
+        else
+        {
+            Console.WriteLine($"Contact {name} Not found in the list");
+        }
+    }
+
+
+
+    public Contact? FindContact(string name)
+    {
+        Contact? findContact = contacts.FirstOrDefault(contactName => contactName.Name == name);
+
+        if (findContact != null)
+        {
+            return findContact;
+        }
+        else
+            return null;
+
+    }
+
+
+
+    public void DisplayContacts()
+    {
+
+        foreach (var item in contacts)
+        {
+            Console.WriteLine($"\t-Name: {item.Name}, Phone: {item.PhoneNumber}, Email: {item.Email}");
+        }
+
+
+
+    }
+}
+
+
+
+
+class App
+{
+
+    static void Main(string[] args)
+    {
+
+        Contact contact;
+
+        ContactManager contactManager = new ContactManager();
+
+
+        contactManager.AddContact(new Contact("Alice Johnson", "123-456-7890", "alice@example.com"));
+        contactManager.AddContact(new Contact("Bob Smith", "098-765-4321", "bob@example.com"));
+        contactManager.AddContact(new Contact("Charlie Brown", "555-555-5555", "charlie@example.com"));
+
+        Console.WriteLine($"\nDisplaying all contacts:");
+        contactManager.DisplayContacts();
+
+        Console.WriteLine($"\nFinding a contact named \"Bob Smith\"");
+        var item = contactManager.FindContact("Bob Smith");
+        Console.WriteLine($"Found: {item}");
+
+
+        Console.WriteLine($"\nRemoving 'Alice Johnson'");
+        contactManager.RemoveContact("Alice Johnson");
+
+        Console.WriteLine($"\nDisplaying contacts after removal");
+        contactManager.DisplayContacts();
+
+
+
+    }
+}
+
+
+
---
+++ GradesManagement-2/Program.cs
@@ -0,0 +1,83 @@
+﻿public record Student(string Name, double Grade, string Subject);
+
+class StudentManager
+{
+    private List<Student> students = new List<Student>();
+
+    public void AddStudent(Student studentX)// Adds a new student to the list.
+    {
+        students.Add(studentX);
+        Console.WriteLine($"Student {studentX.Name} added successfully.");
+
+    }
+
+
+    public void RemoveStudent(string name) // remove student by name
+    {
+
+        Student? studentToRemove = students.FirstOrDefault(studentName => studentName.Name == name);
+
+        if (studentToRemove != null)
+        {
+            students.Remove(studentToRemove);
+            Console.WriteLine($"Student {studentToRemove.Name} Deleted successfully.");
+        }
+        else
+        {
+            Console.WriteLine($"Student {name} not found!");
+        }
+    }
+
+
+    public void DisplayAllStudent()
+    {
+        foreach (var student in students)
+        {
+            Console.WriteLine($"\t-Student {student.Name}: has {student.Grade} in {student.Subject}");
+        }
+    }
+
+
+
+}
+
+
+public class Program
+{
+    
+    static void Main(string[] args)
+    {
+
+        Student student1 = new Student("Aseel",89.9,"ComputerSince");
+        Student student2 = new Student("Sarah",85,"ComputerSince");
+        Student student3 = new Student("Ibrahim",70,"ComputerSince");
+        Student student4 = new Student("Amal",91,"Math");
+
+
+        //Adding Student
+        StudentManager studentManager = new StudentManager();
+        studentManager.AddStudent(student1);
+        studentManager.AddStudent(student2);
+        studentManager.AddStudent(student3);
+        studentManager.AddStudent(student4);
+
+        //Displaying Student
+        Console.WriteLine($"\nDisplaying all Student:");
+        studentManager.DisplayAllStudent();
+        Console.WriteLine($"\n");
+
+        //Removing Student
+        studentManager.RemoveStudent("Amal");
+
+        //Displaying Student
+        Console.WriteLine($"\nDisplaying all Student after removal:");
+        studentManager.DisplayAllStudent();
+        Console.WriteLine($"\n");
+
+
+
+
+
+    }
+
+}
